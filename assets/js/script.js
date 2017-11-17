83cf4417a36f7b9d7e856700ec6c5feb12858d3b
Benjamin <mellecbenjamin@gmail.com>
2017-11-17 16:33:07 +0100

    Add modifications to jQuery animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
 $(".about").hover(function() {
-    $("#cheveux").css("display", "inline");
-    $("#Vector1").css("display", "none");
+    $("#cheveux").css("opacity", "1");
+    $("#cheveux").css("transition", "0.4s");
+    $("#Vector1").css("opacity", "0");
+    $("#Vector1").css("transition", "0.4s");
 }, function() {
-    $("#cheveux").css("display", "none");
-    $("#Vector1").css("display", "inline");
+    $("#cheveux").css("opacity", "0");
+    $("#cheveux").css("transition", "0.4s");
+    $("#Vector1").css("opacity", "1");
+    $("#Vector1").css("transition", "0.4s");
 });
 
 $(".contact").hover(function() {
-    $("#barbe").css("display", "inline");
-    $("#Vector2").css("display", "none");
+    $("#barbe").css("opacity", "1");
+    $("#barbe").css("transition", "0.4s");
+    $("#Vector2").css("opacity", "0");
+    $("#Vector2").css("transition", "0.4s");
 }, function() {
-    $("#barbe").css("display", "none");
-    $("#Vector2").css("display", "inline");
+    $("#barbe").css("opacity", "0");
+    $("#barbe").css("transition", "0.4s");
+    $("#Vector2").css("opacity", "1");
+    $("#Vector2").css("transition", "0.4s");
 });
 
 $(".projects").hover(function() {
-    $("#moustache").css("display", "inline");
-    $("#Vector3").css("display", "none");
+    $("#moustache").css("opacity", "1");
+    $("#moustache").css("transition", "0.4s");
+    $("#Vector3").css("opacity", "0");
+    $("#Vector3").css("transition", "0.4s");
 }, function() {
-    $("#moustache").css("display", "none");
-    $("#Vector3").css("display", "inline");
+    $("#moustache").css("opacity", "0");
+    $("#moustache").css("transition", "0.4s");
+    $("#Vector3").css("opacity", "1");
+    $("#Vector3").css("transition", "0.4s");
 });
