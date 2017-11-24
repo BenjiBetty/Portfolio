39d6a919603540ee2a54d12baf0f0de9730d68ab
Benjamin <mellecbenjamin@gmail.com>
2017-11-24 11:47:47 +0100

    Add the first page of "About me"

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 80,
+            "value": 55,
             "density": {
                 "enable": true,
                 "value_area": 1200
